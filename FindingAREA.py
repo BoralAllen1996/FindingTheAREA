bf7a0f19496b252fcdc7470e7df6a466147d1597
lenbo <allenbora@gmail.com>
2022-12-27 02:06:46 +0800

    Twenty-third

---
+++ FindingAREA.py
@@ -0,0 +1,25 @@
+# Area Calculator 📏
+
+import math
+
+#AREA OF TRIANGLE
+base = 20         
+height = 30
+area = base * height / 2
+
+print("The triangle area is",  area)
+
+#AREA OF RECTANGLE...........
+
+length = 2
+width = 12
+area = length * width
+
+print("The rectangle area is", area )
+    
+ #AREA OF CIRCLE..................   
+radius = 36
+area = math.pi * radius * radius
+    
+print("The circle area is",  area)
+#End.............................
